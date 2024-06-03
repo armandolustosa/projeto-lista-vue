908d33fbcd0b8c868e94a4587514c191892c15a9
Armando Lustosa <115752285+armandolustosa@users.noreply.github.com>
2024-06-02 21:27:16 -0300

    Finalização

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="px-3 py-10 md:px-10">
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+      <TodoSpinner v-if="loading" />
+
+      <template v-else>
+        <TodoForm />
+        <TodoItems />
+        <TodoEmpty v-if="!$store.state.todos.length" />
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoForm from "./components/TodoForm.vue";
+import TodoItems from "./components/TodoItems.vue";
+import TodoSpinner from "./components/TodoSpinner.vue";
+import TodoEmpty from "./components/TodoEmpty.vue";
+import { ref } from "vue";
+import { useStore } from "vuex";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    TodoSpinner,
+    TodoForm,
+    TodoItems,
+    TodoEmpty,
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  setup() {
+    const loading = ref(false);
+
+    const store = useStore();
+
+    loading.value = true;
+    store.dispatch("getTodos").finally(() => {
+      loading.value = false;
+    });
+
+    return {
+      loading,
+    };
+  },
+};
+</script>
